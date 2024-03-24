4cf19c4e83da3c7c232754bdcd6ca5cd9763a3dc
cje98 <chlwldms120@naver.com>
2024-03-24 22:18:19 +0900

    WebRTC로 화상채팅 만들기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -6,9 +6,15 @@ const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 
+const call = document.getElementById("call");
+
+call.hidden = true;
+
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
 
 async function getCameras() {
     try{
@@ -54,8 +60,6 @@ async function getMedia(deviceId) {
     }
 }
 
-getMedia();
-
 function handleMuteClick() {
     myStream
         .getAudioTracks()
@@ -84,10 +88,111 @@ function handleCameraClick () {
 }
 
 // camera switch
+// Sender : peer로 보내진 media stream track을 컨트롤 하게 해줌
 async function handleCameraChange() {
     await getMedia(camerasSelect.value);
+    if (myPeerConnection){
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection
+                .getSenders()
+                .find(sender => sender.track.kind === "video");
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form (join a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+// startMedia : media 가져가서 연결을 만들어 주는 함수
+async function initCall() {
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+
+async function handleWelcomeSubmit(event) {
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    await initCall();
+    socket.emit("join_room", input.value);
+    roomName = input.value;
+    input.value = "";
+}
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+// Peer A
+socket.on("welcome", async () => {
+    // 다른 브라우저에서 접속할 때 실행됨, 서버 필요함
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    console.log("sent the offer");
+    socket.emit("offer", offer, roomName);
+});
+
+// Peer B
+// offer 받고 peer description 생성
+socket.on("offer", async(offer) => {
+    console.log("received the offer");
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+    console.log("sent the answer");
+});
+
+// Peer A
+// answer
+socket.on("answer", answer => {
+    console.log("received the answer");
+    myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", ice => {
+    console.log("received candidate");
+    myPeerConnection.addIceCandidate(ice);
+});
+
+// RTC Code
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection({
+        iceServers:[
+            {
+                // STUN 서버 : 공용 주소를 알려주는 서버
+                url: [
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302",
+                    "stun:stun3.l.google.com:19302",
+                    "stun:stun4.l.google.com:19302",
+                ],
+            },
+        ],
+    });
+    // myStream.getTracks() : 영상과 비디오 데이터 가져오는 메소드
+    // 각 브라우저에 따로 구성
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+    myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+// iceCandidate : webRTC에 필요한 프로토콜, 멀리 떨어진 장치와 소통할 수 있게 해줌
+// 브라우저끼리 주고받음
+function handleIce(data) {
+    console.log("sent candidate");
+    socket.emit("ice", data.candidate, roomName);
+}
+
+// peer 브라우저에서 비디오, 오디오 데이터를 컨트롤 할 수 있음(remote stream)
+function handleAddStream(data) {
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+}
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import http from 'http';
-import WebSocket from 'ws';
 import SocketIO from 'socket.io';
 
 const app = express();
@@ -12,6 +11,22 @@ app.get("/", (req, res) => res.render("home"));
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    });
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
+    })
+})
 
 const handleListen = () => console.log(`Listenig on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
